9dcbfb838b095c53aec97dd849f2224cf2fe5ad7
tucecifci <tucecifcii@gmail.com>
2025-10-15 20:56:13 +0300

    Update app layout to include new screens for navigation and replace index screen with StartGameScreen. Removed obsolete GameOverScreen, GameScreen, and StartGameScreen files to clean up the project structure.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,12 @@
 import { Stack } from "expo-router";
 
+
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Stack>
+      <Stack.Screen name="start-game" />
+      <Stack.Screen name="game" />
+      <Stack.Screen name="game-over" />
+    </Stack>
+  );
 }
