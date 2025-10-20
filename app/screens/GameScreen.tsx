ad000f248c006ea737f8772a07828645474dd3d4
tucecifci <tucecifcii@gmail.com>
2025-10-20 21:42:42 +0300

    Implement game logic in Home component and enhance StartGameScreen with input validation and number selection.

--- app/screens/GameScreen.tsx
+++ app/screens/GameScreen.tsx
@@ -1,14 +1,14 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
 
 const GameScreen = () => {
   return (
     <View>
       <Text>GameScreen</Text>
     </View>
-  )
-}
+  );
+};
 
-export default GameScreen
+export default GameScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
